18d907d029d6512e7d15c924f887a20f7a354de4
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-25 21:25:14 +0900

    ColumbusService: TableDetectionGate: Clean-up codes
    Change-Id: I9744aacdd55c5542b1152f990f6a1d02538593ae

--- src/org/protonaosp/columbus/gates/TableDetectionGate.kt
+++ src/org/protonaosp/columbus/gates/TableDetectionGate.kt
@@ -10,8 +10,9 @@ import kotlin.math.acos
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
-// Loosely based on
+// References:
 // https://stackoverflow.com/questions/30948131/how-to-know-if-android-device-is-flat-on-table
+// https://stackoverflow.com/questions/11175599/how-to-measure-the-tilt-of-the-phone-in-xy-plane-using-accelerometer-in-android/15149421#15149421
 class TableDetectionGate(context: Context, val handler: Handler) : Gate(context, handler, 2) {
 
     private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -26,16 +27,14 @@ class TableDetectionGate(context: Context, val handler: Handler) : Gate(context,
                 val values = event?.values ?: return
 
                 // Movement
-                var x = values[0]
-                var y = values[1]
-                var z = values[2]
-                val norm = sqrt(x * x + y * y + (z * z).toDouble()).toFloat()
+                var x = values[0].toDouble()
+                var y = values[1].toDouble()
+                var z = values[2].toDouble()
+                val norm = sqrt(x * x + y * y + z * z)
 
                 // Normalize the accelerometer vector
-                x /= norm
-                y /= norm
                 z /= norm
-                val inclination = Math.toDegrees(acos(z.toDouble())).roundToInt()
+                val inclination = Math.toDegrees(acos(z)).roundToInt()
 
                 val isFlat = inclination < 3 || inclination > 177
                 if (isFlat != this.isFlat) {
