7d6cf6ca1c7f3ffdd10bb7f5db0d288866b18cc6
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-02-09 01:46:15 +0900

    ColumbusService: Update UsbState gate
    Change-Id: Ife4af4e7bd5fe3ecc99dd87b099cfed9a9cebbda

--- src/org/protonaosp/columbus/gates/UsbState.kt
+++ src/org/protonaosp/columbus/gates/UsbState.kt
@@ -9,29 +9,29 @@ import android.os.Handler
 
 class UsbState(context: Context, handler: Handler) : TransientGate(context, handler) {
     private var usbConnected: Boolean = false
-    private val usbReceiver: BroadcastReceiver =
+
+    private val usbReceiver =
         object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                if (intent == null) {
-                    return
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val connected = intent?.getBooleanExtra(UsbManager.USB_CONNECTED, false) ?: return
+
+                if (connected != usbConnected) {
+                    usbConnected = connected
+                    if (connected) {
+                        blockForMillis(GATE_DURATION)
+                    } else {
+                        setBlocking(false)
+                    }
                 }
-                val connected: Boolean = intent.getBooleanExtra(UsbManager.USB_CONNECTED, false)
-                if (connected == usbConnected) return
-                usbConnected = connected
-                blockForMillis(GATE_DURATION)
             }
         }
 
     override fun onActivate() {
-        val intentFilter: IntentFilter = IntentFilter(UsbManager.ACTION_USB_STATE)
-        val receiver: Intent? = context.registerReceiver(null, intentFilter)
-        if (receiver != null) {
-            usbConnected = receiver!!.getBooleanExtra(UsbManager.USB_CONNECTED, false)
-        }
-        context.registerReceiver(usbReceiver, intentFilter)
+        context.registerReceiver(usbReceiver, IntentFilter(UsbManager.ACTION_USB_STATE))
     }
 
     override fun onDeactivate() {
         context.unregisterReceiver(usbReceiver)
+        setBlocking(false)
     }
 }
