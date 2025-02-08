aecca3e857824bc019a1a687215ea77e5e6b9922
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-02-09 01:42:57 +0900

    ColumbusService: Update TelephonyActivity gate
    Change-Id: I09777e6c8629d2436791c1e53710580091c19980

--- src/org/protonaosp/columbus/gates/TelephonyActivity.kt
+++ src/org/protonaosp/columbus/gates/TelephonyActivity.kt
@@ -2,6 +2,7 @@ package org.protonaosp.columbus.gates
 
 import android.content.Context
 import android.os.Handler
+import android.telecom.TelecomManager
 import android.telephony.TelephonyCallback
 import android.telephony.TelephonyManager
 import java.util.concurrent.Executor
@@ -9,9 +10,14 @@ import java.util.concurrent.Executor
 class TelephonyActivity(context: Context, handler: Handler) : Gate(context, handler, 2) {
     private var callBlocked: Boolean = false
 
+    private val telephonyManager: TelephonyManager? =
+        context.getSystemService(Context.TELEPHONY_SERVICE) as? TelephonyManager
+    private val telecomManager: TelecomManager? =
+        context.getSystemService(Context.TELECOM_SERVICE) as? TelecomManager
+
     inner class PhoneCallback : TelephonyCallback(), TelephonyCallback.CallStateListener {
         override fun onCallStateChanged(state: Int) {
-            callBlocked = isCallBlocked(state)
+            callBlocked = telecomManager?.let { it.isInCall() } ?: false
             updateBlocking()
         }
     }
@@ -19,24 +25,21 @@ class TelephonyActivity(context: Context, handler: Handler) : Gate(context, hand
     private val phoneCallback: PhoneCallback = PhoneCallback()
     private val executor: Executor = context.mainExecutor
 
-    private val telephonyManager: TelephonyManager =
-        context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-
-    fun isCallBlocked(state: Int): Boolean {
-        return state != null && state == TelephonyManager.CALL_STATE_OFFHOOK
-    }
-
     fun updateBlocking() {
         setBlocking(callBlocked)
     }
 
     override fun onActivate() {
-        callBlocked = isCallBlocked(telephonyManager.getCallState())
-        telephonyManager.registerTelephonyCallback(executor, phoneCallback)
+        telephonyManager?.let { tm ->
+            callBlocked = telecomManager?.let { it.isInCall() } ?: false
+
+            tm.registerTelephonyCallback(executor, phoneCallback)
+        }
         updateBlocking()
     }
 
     override fun onDeactivate() {
-        telephonyManager.unregisterTelephonyCallback(phoneCallback)
+        setBlocking(false)
+        telephonyManager?.unregisterTelephonyCallback(phoneCallback)
     }
 }
