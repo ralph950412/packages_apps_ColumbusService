dd43108f22b7bb9a82f14231cf1a02a5466321b5
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-02-19 17:05:33 +0900

    ColumbusService: Update SystemKeyPress gate
    Change-Id: Ie00be51e2d89cd7b016b457f551eefbf954b1d98

--- src/org/protonaosp/columbus/gates/SystemKeyPress.kt
+++ src/org/protonaosp/columbus/gates/SystemKeyPress.kt
@@ -11,58 +11,71 @@ import com.android.systemui.shared.system.InputChannelCompat
 import com.android.systemui.shared.system.InputMonitorCompat
 import org.protonaosp.columbus.TAG
 
-class SystemKeyPress(context: Context, handler: Handler) : TransientGate(context, handler) {
+class SystemKeyPress(context: Context, handler: Handler) : Gate(context, handler, 2) {
+    private val clearBlocking = Runnable { setBlocking(false) }
     private val blockingKeys =
         setOf(KeyEvent.KEYCODE_VOLUME_UP, KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_POWER)
 
     private var inputEventReceiver: InputChannelCompat.InputEventReceiver? = null
     private var inputMonitor: InputMonitorCompat? = null
-    private var isListening: Boolean = false
-
     private val inputEventListener =
         object : InputChannelCompat.InputEventListener {
             override fun onInputEvent(ev: InputEvent) {
+                if (ev !is KeyEvent) {
+                    return
+                }
                 val keyEvent: KeyEvent = ev as? KeyEvent ?: return
+                if (!isBlockingKeys(keyEvent)) return
 
-                inputEventReceiver?.let {
-                    if (isBlockingKeys(keyEvent)) {
-                        blockForMillis(GATE_DURATION)
+                when (keyEvent.action) {
+                    KeyEvent.ACTION_DOWN -> {
+                        setBlocking(true)
+                    }
+                    KeyEvent.ACTION_UP -> {
+                        handler.removeCallbacks(clearBlocking)
+                        handler.postDelayed(clearBlocking, GATE_DURATION)
                     }
                 }
             }
         }
 
     private fun dispose() {
-        inputEventReceiver?.dispose()
-        inputMonitor?.dispose()
+        if (inputEventReceiver != null) {
+            inputEventReceiver!!.dispose()
+        }
+        if (inputMonitor != null) {
+            inputMonitor!!.dispose()
+        }
     }
 
     private fun isBlockingKeys(keyEvent: KeyEvent): Boolean {
         return blockingKeys.contains(keyEvent.keyCode)
     }
 
-    override fun onActivate() {
-        if (isListening) return
-
+    fun startListeningForKeyPress() {
+        if (inputEventReceiver != null) return
         inputMonitor = InputMonitorCompat(TAG, 0)
         inputEventReceiver =
-            inputMonitor?.getInputReceiver(
+            inputMonitor!!.getInputReceiver(
                 Looper.getMainLooper(),
                 Choreographer.getInstance(),
                 inputEventListener,
             )
-
-        isListening = inputEventReceiver != null
-        if (isListening) {
-            setBlocking(false)
-        }
     }
 
-    override fun onDeactivate() {
-        isListening = false
+    fun stopListeningForKeyPress() {
         setBlocking(false)
         dispose()
         inputEventReceiver = null
         inputMonitor = null
+    }
+
+    override fun onActivate() {
+        startListeningForKeyPress()
+        setBlocking(false)
+    }
+
+    override fun onDeactivate() {
+        stopListeningForKeyPress()
     }
 }
